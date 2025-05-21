717e1e0a588dea06a22cf3700f65e784c457d3ef
Harshdadhwal <83348325+Harshdadhwal@users.noreply.github.com>
2025-05-21 19:15:44 +0530

    header and footer dynamically check

--- assets/load-analytics.js
+++ assets/load-analytics.js
@@ -15,3 +15,21 @@
         gtag('config', 'G-6JPEMKWNPK');
     }
 })();
+
+
+
+
+
+// Load header
+fetch('/components/header.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('header').innerHTML = data;
+  });
+
+// Load footer
+fetch('/components/footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('footer').innerHTML = data;
+  });
