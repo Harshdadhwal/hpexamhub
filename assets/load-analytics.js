168793ff5c2c040d27db8fcc954822d3ed1a9b23
Harshdadhwal <83348325+Harshdadhwal@users.noreply.github.com>
2025-05-21 19:25:46 +0530

    new

--- assets/load-analytics.js
+++ assets/load-analytics.js
@@ -18,18 +18,3 @@
 
 
 
-
-
-// Load header
-fetch('/components/header.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('header').innerHTML = data;
-  });
-
-// Load footer
-fetch('/components/footer.html')
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById('footer').innerHTML = data;
-  });
